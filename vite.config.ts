003ee4fb6e885bbaea873bb7b51e6f28bcead6c6
Shubham Pardeshi <shubhampardeshi11@gmail.com>
2025-06-13 13:02:49 +0530

    updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,20 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    lib: {
-      entry: 'src/main.tsx',
-      name: 'ChatbotEmbed',
-      fileName: (format) => `chatbot-embed.${format}.js`,
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
-      },
-    },
-  },
 })
